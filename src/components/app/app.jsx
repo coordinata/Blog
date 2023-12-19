ff8ae0264c1880a4b60159490645dd7c8db6a7b9
Margarita <sempai_m@mail.ru>
2023-12-20 00:24:59 +0300

    .

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 // import classes from './app.module.scss'
-
+import Header from "../header/header";
 
 const App = () => {
-    return(<div>mrmrm</div>)
+    return(<div>
+        <Header/>
+    </div>)
 }
 
 export default App
---
+++ src/components/header/header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./header.module.scss";
+
+const Header = () => {
+  return <div className={classes.header}>ppp</div>;
+};
+
+export default Header;
