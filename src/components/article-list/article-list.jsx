c41524f94d08dfe0b02b1e6427dcba27ab9ad6d1
Margarita <sempai_m@mail.ru>
2023-12-20 22:39:09 +0300

    .

--- src/components/article-list/article-list.jsx
+++ src/components/article-list/article-list.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Article from "../article/article";
-import classes from './article-list.module.scss'
+import classes from "./article-list.module.scss";
+import { Pagination } from "antd";
 
 const ArticleList = () => {
   return (
     <div className={classes.wrapper}>
-      <Article/>
-      <Article/>
-      <Article/>
-      <Article/>
-      <Article/>
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Pagination className={classes.pagination} total={50} />
     </div>
   );
 };
