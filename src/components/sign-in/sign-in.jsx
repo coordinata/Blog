7ecaf49d9c1bdbd0c835704a83e3ca02bd0b5d85
Margarita <sempai_m@mail.ru>
2023-12-27 22:22:18 +0300

    .

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import classes from "./sign-in.module.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { postLoginUser } from "../../store/user-slice";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const loginUser = () => {
+    dispatch(postLoginUser({email, password}));
+  };
+
+  const changeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const changePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div className={classes.wrapper}>
       <h1 className={classes.title}>Sign In</h1>
@@ -10,6 +30,7 @@ const SignIn = () => {
         <label className={classes.email}>
           Email address
           <input
+            onChange={changeEmail}
             className={classes.email_input}
             type="email"
             placeholder="Email address"
@@ -18,15 +39,21 @@ const SignIn = () => {
         <label className={classes.password}>
           Password
           <input
+          onChange={changePassword}
             className={classes.password_input}
             type="password"
             placeholder="Password"
           />
         </label>
       </form>
-      <button className={classes.login_btn}>Login</button>
+      <button className={classes.login_btn} onClick={loginUser}>
+        Login
+      </button>
       <p className={classes.text}>
-        Don’t have an account? <Link to="/sign-up" className={classes.text_sign_up}>Sign Up.</Link>
+        Don’t have an account?{" "}
+        <Link to="/sign-up" className={classes.text_sign_up}>
+          Sign Up.
+        </Link>
       </p>
     </div>
   );
--- src/store/user-slice.jsx
+++ src/store/user-slice.jsx
@@ -2,17 +2,31 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-  createUser: false,
+  loginUser: false,
   loading: false,
   error: false,
 };
 
 export const postCreateUser = createAsyncThunk(
   "createUser/postCreateUser",
-  async ({name, email, password}) => {
+  async ({ name, email, password }) => {
     const res = await axios.post("https://blog.kata.academy/api/users/", {
       user: {
         username: name,
+        email: email,
+        password: password,
+      },
+    });
+    console.log(res.data);
+    return res.data;
+  }
+);
+
+export const postLoginUser = createAsyncThunk(
+  "loginUser/postLoginUser",
+  async ({email, password}) => {
+    const res = await axios.post("https://blog.kata.academy/api/users/login", {
+      user: {
         email: email,
         password: password,
       },
@@ -27,7 +41,10 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     setCreateUser: (state, action) => {
-      state.createUser = true;
+      state.loginUser = true;
+    },
+    setLoginUser: (state, action) => {
+      state.loginUser = true;
     },
   },
   extraReducers: {
@@ -37,13 +54,26 @@ export const userSlice = createSlice({
     },
     [postCreateUser.fulfilled]: (state, action) => {
       state.loading = false;
+      state.loginUser = true;
     },
     [postCreateUser.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = true;
+    },
+    [postLoginUser.pending]: (state, action) => {
+      state.loading = true;
+      state.error = false;
+    },
+    [postLoginUser.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.loginUser = true;
+    },
+    [postLoginUser.rejected]: (state, action) => {
       state.loading = false;
       state.error = true;
     },
   },
 });
 
-export const { createUser } = userSlice.actions;
+export const { createUser, loginUser } = userSlice.actions;
 export default userSlice.reducer;
