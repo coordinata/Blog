2d9706d45ec2539b7e7f74a93834c03be9c48874
Margarita <sempai_m@mail.ru>
2024-01-06 19:37:19 +0300

    .

--- src/components/create-article/create-article.jsx
+++ src/components/create-article/create-article.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./create-article.module.scss";
 import { useDispatch } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useForm } from "react-hook-form";
 import { createArticle } from "../../store/article-slice";
-
+import { useRef } from "react";
 
 const CreateArticle = () => {
-  const notify = () => toast.success("article created successfully!");
+  const [tagsArr, setTagsArr] = useState([]);
+
+  const notify = () => toast.success("Article created successfully!");
   const dispatch = useDispatch();
+
+  const tagInputRef = useRef(null);
+
+  const addTag = (e) => {
+    e.preventDefault()
+    if (tagInputRef.current.value.trim() !== "") {
+      setTagsArr([...tagsArr, tagInputRef.current.value]);
+    }
+    tagInputRef.current.value = ""; // Очищаем значение инпута после добавления в массив
+  };
+
+  const removeTag = (index) => {
+    const updatedTagsArr = tagsArr.filter((_, i) => i !== index);
+    setTagsArr(updatedTagsArr);
+  };
+
+  const onSubmit = (data) => {
+    dispatch(createArticle(data));
+    reset();
+    notify();
+  };
+
+  const qqq = () => {
+    console.log(tagsArr);
+  };
 
   const {
     register,
@@ -20,18 +47,12 @@ const CreateArticle = () => {
     mode: "onChange",
   });
 
-  const onSubmit = (data) => {
-    dispatch(createArticle(data));
-    reset();
-    notify();
-  };
-
   return (
     <div className={classes.wrapper}>
       <h1 className={classes.title}>Create new article</h1>
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label className={classes.title_article}>
+        <label className={classes.title_article} onClick={qqq()}>
           Title
           <input
             {...register("title", {
@@ -68,7 +89,6 @@ const CreateArticle = () => {
               required: "This field is required!",
             })}
             className={classes.text_input}
-            required
             placeholder="Text"
           />
           <div className={classes.error}>
@@ -79,9 +99,34 @@ const CreateArticle = () => {
         <label className={classes.tags}>
           Tags
           <div className={classes.tag_wrapper}>
-            <input className={classes.tag} type="text" placeholder="Tag" />
-            <button className={classes.del_btn}>Delete</button>
-            <button className={classes.add_btn}>Add tag</button>
+            {tagsArr.map((tag, index) => (
+              <div key={index} className={classes.tag_row}>
+                <input
+                  className={classes.tag}
+                  type="text"
+                  placeholder="Tag"
+                  value={tag}
+                  readOnly
+                />
+                <button
+                  className={classes.del_btn}
+                  onClick={() => removeTag(index)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+            <div className={classes.tag_row}>
+              <input
+                ref={tagInputRef}
+                className={classes.tag}
+                type="text"
+                placeholder="Tag"
+              />
+              <button className={classes.add_btn} onClick={(e) => addTag(e)}>
+                Add tag
+              </button>
+            </div>
           </div>
         </label>
 
